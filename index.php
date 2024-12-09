3f2b097d135040f35934203beabacdf57abbc291
thaonguyenbi <thaonguyen.31221023052@st.ueh.edu.vn>
2024-12-09 16:31:59 +0700

    fix header

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ if (isset($_SESSION['success_message'])) {
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-
+  <link href="css/cart.css" rel="stylesheet">
 </head>
 
 <body>
